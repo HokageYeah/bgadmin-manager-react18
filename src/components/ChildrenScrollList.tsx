49b3cdc00e9ef179a53145a8ea3db27c763e185b
余晔 <18838280615@163.com>
2023-05-26 00:23:01 +0800

    虚拟滚动列表方案1:渲染可视区域内的数据

--- src/components/ChildrenScrollList.tsx
+++ src/components/ChildrenScrollList.tsx
@@ -1,33 +1,68 @@
-import React, { memo, useRef } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import classNames from 'classnames';
 
 interface propsType {
-  yuye: number[];
+  yuye: any[];
 }
 const StyledDiv = styled.div`
   /* 样式属性 */
-  height: 288px;
+  height: 380px;
   width: 30%;
   margin: 20px auto;
   overflow: scroll;
   border: 1px solid black;
+  .virtual-list-phantom {
+    float: left;
+  }
+  .virtual-list-item {
+    box-sizing: border-box;
+    height: 30px;
+    margin: 10px 10px 0px 10px;
+    border: 1px solid red;
+  }
 `;
 const Children = memo((props: propsType) => {
   const { yuye } = props;
+  const virtualRef = useRef(null);
   const virtualContentRef = useRef(null);
+  const [isShowItem] = useState(true);
+  const [start, setStart] = useState(0);
+  const [visibleCount, setVisibleCount] = useState(0);
+  // 使用泛型指定类型为 string[]
+  const [visiblData, setVisibleData] = useState<number[]>([]);
   const onHandleScroll = () => {
-    const scrolltop = (virtualContentRef.current as unknown as HTMLDivElement).scrollTop;
-    console.log(scrolltop);
+    const scrolltop = (virtualRef.current as unknown as HTMLDivElement).scrollTop;
+    const fixedScrollTop = scrolltop - (scrolltop % 40);
+    (
+      virtualContentRef.current as unknown as HTMLDivElement
+    ).style.webkitTransform = `translate3d(0, ${fixedScrollTop}px, 0)`;
+    setStart(Math.floor(scrolltop / 40));
+    setVisibleData(yuye.slice(start, start + visibleCount));
+    console.log(scrolltop, fixedScrollTop);
   };
+  // []空数组表示谁的影响都不受，只会首次执行一次
+  useEffect(() => {
+    // 获取父元素的高度 这里使用c3的盒模型
+    const height = (virtualRef.current as unknown as HTMLDivElement).clientHeight;
+    // 获取屏幕能够显示多少个元素
+    const count = Math.ceil(height / 40);
+    // 获取首次加载屏幕内元素
+    setVisibleCount(count);
+    setVisibleData(yuye.slice(start, start + count));
+    console.log(height, count, visibleCount);
+  }, []);
   const clickBtn = () => {
     console.log('点击可大量的卡上了');
   };
   return (
-    <StyledDiv className="virtual-list" ref={virtualContentRef} onScroll={onHandleScroll} onClick={clickBtn}>
-      {/* <div className="virtual-list-phantom" style={{ height: yuye.length * 30 + 'px' }}></div> */}
-      <div>
-        {yuye.map((item) => (
-          <div key={item}>---{item}</div>
+    <StyledDiv ref={virtualRef} onScroll={onHandleScroll} onClick={clickBtn}>
+      <div className="virtual-list-phantom" style={{ height: yuye.length * 40 + 'px' }}></div>
+      <div ref={virtualContentRef}>
+        {visiblData.map((item) => (
+          <div className={classNames({ 'virtual-list-item': isShowItem })} key={item}>
+            ---{item}
+          </div>
         ))}
       </div>
     </StyledDiv>
