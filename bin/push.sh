6b5daa3829b2231e93f15ff8c88f2f7b59cca535
yuye <18838280615@163.com>
2023-05-26 15:16:23 +0800

    开发分支dev2

--- bin/push.sh
+++ bin/push.sh
@@ -40,7 +40,7 @@ exec_project(){
     git add -A  2>>$log
     git commit -m "$git_message"
     git push origin "$main_branch"
-    echo "主分支推送成功！🎉🎉🎉"
+    echo "$main_branch 分支推送成功！🎉🎉🎉"
 }
 
 message_check
