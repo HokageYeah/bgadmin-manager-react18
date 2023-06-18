993f5f6b39fe227dd9e6fb23ef6ff45f64dfec0a
余晔 <18838280615@163.com>
2023-06-18 17:51:08 +0800

    样式部分代码t提交

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,13 +1,21 @@
-import { useAppSelector } from '@/store';
-import React, { memo } from 'react';
-import type { ReactNode, FC } from 'react';
+import React, { memo, useRef, useState } from 'react';
+import type { ReactNode, FC, ElementRef } from 'react';
 import { shallowEqual } from 'react-redux';
+
+import { useAppSelector } from '@/store';
+import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style';
+import { Carousel } from 'antd';
+import classNames from 'classnames';
 
 interface IProps {
   childrens?: ReactNode;
 }
 
 const TopBanner: FC<IProps> = () => {
+  //定义内部数据
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null);
+
   // 从store中获取数据
   const { bannedrs } = useAppSelector(
     (state) => ({
@@ -15,12 +23,70 @@ const TopBanner: FC<IProps> = () => {
     }),
     shallowEqual //浅层比较，如果一样就调用render了
   );
+
+  /** 事件处理函数 */
+  const handleBeforeChange = () => {
+    // setCurrentIndex(-1)
+  };
+  const handleAfterChange = (current: number) => {
+    setCurrentIndex(current);
+    console.log(current);
+  };
+  const handlePrevClick = () => {
+    bannerRef.current?.prev();
+    console.log('handlePrevClick');
+  };
+  const handleNextClick = () => {
+    bannerRef.current?.next();
+    console.log('handleNextClick');
+  };
+  /** 获取背景图片 */
+  let bgImageUrl;
+  if (currentIndex >= 0 && bannedrs.length > 0) {
+    bgImageUrl = bannedrs[currentIndex].imageUrl + '?imageView&blur=40x20';
+  }
   return (
-    <div>
-      {bannedrs.map((item: any) => {
-        return <div key={item.imageUrl}>哈哈哈{item.imageUrl}</div>;
-      })}
-    </div>
+    <BannerWrapper
+      style={{
+        background: `url('${bgImageUrl}') center center / 6000px`
+      }}
+    >
+      <div className="banner wrap-v2">
+        <BannerLeft>
+          <Carousel
+            autoplay
+            dots={false}
+            autoplaySpeed={3000}
+            ref={bannerRef}
+            effect="fade"
+            beforeChange={handleBeforeChange}
+            afterChange={handleAfterChange}
+          >
+            {bannedrs.map((item: any) => {
+              return (
+                <div key={item.imageUrl} className="banner-item">
+                  <img className="image" src={item.imageUrl} alt={item.typeTitle} />
+                </div>
+              );
+            })}
+          </Carousel>
+          <ul className="dots">
+            {bannedrs.map((item: any, index: number) => {
+              return (
+                <li key={item.imageUrl}>
+                  <span className={classNames('item', { active: index == currentIndex })}></span>
+                </li>
+              );
+            })}
+          </ul>
+        </BannerLeft>
+        <BannerRight></BannerRight>
+        <BannerControl>
+          <button className="btn left" onClick={handlePrevClick}></button>
+          <button className="btn right" onClick={handleNextClick}></button>
+        </BannerControl>
+      </div>
+    </BannerWrapper>
   );
 };
 
